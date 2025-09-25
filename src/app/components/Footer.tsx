461a5c02e4bc398804ae7ab23c2cb8264beb5fb9
Taimoor Hassan <taimoorhassansamiullahjan@gmail.com>
2025-09-25 11:43:09 +0500

    Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,13 +6,13 @@ const Footer = () => {
   return (
     <footer className="bg-[#111827] text-gray-300 py-6 text-center">
       <div className="flex justify-center gap-6 mb-4">
-        <Link href="https://github.com/taimoor-hassan01" target="_blank" rel="noopener noreferrer" className="w-10 h-10">
+        {/* <Link href="https://github.com/taimoor-hassan01" target="_blank" rel="noopener noreferrer" className="w-10 h-10"> */}
           <Image src="/github.png" alt="GitHub" className="w-full h-full" width={300} height={300}/>
         </Link>
-        <Link href="https://www.linkedin.com/in/dosto-ki-dunya-taimoor-207568317/" target="_blank" rel="noopener noreferrer" className="w-10 h-10">
+        {/* <Link href="https://www.linkedin.com/in/dosto-ki-dunya-taimoor-207568317/" target="_blank" rel="noopener noreferrer" className="w-10 h-10"> */}
           <Image src="/linkedin.png" alt="LinkedIn" className="w-full h-full" width={300} height={300}/>
         </Link>
-        <Link href="https://www.facebook.com/taimoor.samiullahjan/" target="_blank" rel="noopener noreferrer" className="w-10 h-10">
+        // <Link href="https://www.facebook.com/taimoor.samiullahjan/" target="_blank" rel="noopener noreferrer" className="w-10 h-10">
           <Image src="/facebook.png" alt="Facebook" className="w-full h-full" width={300} height={300}/>
         </Link>
       </div>
